ce0d4b48b68276535e13e5ec6f40763ee3c101bc
Yu-Hsin(Henry) Chen <yu-hsin.chen@emory.edu>
2015-01-06 20:48:08 +0800

    Updaet Dynamic.Hanoi package

--- src/main/java/edu/emory/mathcs/cs323/dynamic/hanoi/DHanoi.java
+++ src/main/java/edu/emory/mathcs/cs323/dynamic/hanoi/DHanoi.java
@@ -40,17 +40,27 @@ public class DHanoi extends AbstractHanoi
 		if (n == 0) return;
 		int fromIndex = list.size();
 		
+		//Get previous steps to move current plate to designate destination
 		int[] sub = map.get(getKey(n-1, source, intermediate));
+		
+		//If previous steps exist, add the steps to the step list
 		if (sub != null)	addAll(list, sub[0], sub[1]);
+		//Move all plates from 'source' to 'intermediate' via 'destination' as medium
 		else				solve(list, n-1, source, destination, intermediate, map);		
 		
+		//Record the step
 		String key = getKey(n, source, destination);
 		list.add(key);
 		
+		//Get previous steps to move current plate to designate destination
 		sub = map.get(getKey(n-1, intermediate, destination));
+		
+		//If previous steps exist, add the steps to the step list
 		if (sub != null)	addAll(list, sub[0], sub[1]);
+		//Move all plates from 'intermediate' to 'destination' via 'source' as medium
 		else				solve(list, n-1, intermediate, source, destination, map);
 		
+		//If steps do not exist, add them to the dynamic list
 		if (!map.containsKey(key))
 			map.put(key, new int[]{fromIndex, list.size()});
 	}
