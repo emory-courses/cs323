e22adade986c42ef4e16f94694005ca43058a2be
Henry Chen <yuhsinchen19940823@gmail.com>
2015-09-18 00:22:07 -0400

    Rename lab3

--- src/main/java/lab/lab3_20150917/InterfacePractice.java
+++ src/main/java/lab/lab3_20150917/InterfacePractice.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package lab.lab_20150917;
+package lab.lab3_20150917;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
